4e8ca979217b1fbffcd1adf377eb2d1ee0f54d31
kostasflu <kostasflu@gmail.com>
2016-10-21 12:50:59 +0300

    new

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,35 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        System.out.println("Hello World\n");
+        classA obj=new classA();
+        obj.a=1;
+        obj.b=12.3f;
+        System.out.println(obj.a+"\n"+obj.b+"\n");
+        obj.setA(2);
+        obj.setB(15.23f);
+        System.out.println(obj.getA()+"\n"+obj.getB()+"\n");
+        
+        
     }
     
+}
+
+ class classA{
+    int a;
+    float b;
+    
+     int getA(){
+        return a;
+    }
+     float getB(){
+        return b;
+    }
+     
+     void setA(int x){
+        a=x;
+    }
+     void setB(float x){
+        b=x;
+    }
 }
